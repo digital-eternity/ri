25856d3ef4ff61911973749c326eb047eada1b23
digital-eternity <Swinjoc123>
2025-09-04 23:54:34 +0300

    Added sounds

--- backend/src/main/java/com/ri/model/preset/Preset.java
+++ backend/src/main/java/com/ri/model/preset/Preset.java
@@ -2,13 +2,33 @@ package com.ri.model.preset;
 
 import java.util.List;
 
-public class RIPreset
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Preset
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	protected String name;
 	protected List<String> elements;
 		
-	protected RIPreset( )
+	protected Preset( )
 	{
+	}
+	
+	public Long getId() 
+	{
+		return id;
+	}
+	
+	public void setId( Long id ) 
+	{
+		this.id = id;
 	}
 
 	public String getName()
